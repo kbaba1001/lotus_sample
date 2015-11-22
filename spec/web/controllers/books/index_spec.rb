888ac0e5e38389254632f53fecb3816ae55c288b
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-11-22 13:56:35 +0900

    show db data on view

--- spec/web/controllers/books/index_spec.rb
+++ spec/web/controllers/books/index_spec.rb
@@ -2,10 +2,21 @@ require_relative '../../../../apps/web/controllers/books/index'
 
 describe Web::Controllers::Books::Index do
   let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  let(:params) { {} }
+
+  before do
+    BookRepository.clear
+
+    @book = BookRepository.create(Book.new(title: 'TDD', author: 'Kent Beck'))
+  end
 
   it 'is successful' do
     response = action.call(params)
     expect(response[0]).to eq 200
+  end
+
+  it 'exposes all books' do
+    action.call(params)
+    expect(action.exposures[:books]).to eq([@book])
   end
 end
--- spec/web/views/books/index_spec.rb
+++ spec/web/views/books/index_spec.rb
@@ -28,7 +28,7 @@ describe Web::Views::Books::Index do
     end
 
     it 'hides the placeholder message' do
-      expect(rendered).to include('<p class="placeholder">There are no books yet.</p>')
+      expect(rendered).not_to include('<p class="placeholder">There are no books yet.</p>')
     end
   end
 end
