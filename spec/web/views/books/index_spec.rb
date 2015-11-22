6bd95b4c96399a369e0db5ed45a92df9ba33be6c
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-11-22 12:19:12 +0900

    fix spec/web/views/books/index_spec.rb

--- spec/web/views/books/index_spec.rb
+++ spec/web/views/books/index_spec.rb
@@ -1,12 +1,12 @@
 require_relative '../../../../apps/web/views/books/index'
 
 describe Web::Views::Books::Index do
-  let(:exposures) { Hash[foo: 'bar'] }
+  let(:exposures) { {books: []} }
   let(:template)  { Lotus::View::Template.new('apps/web/templates/books/index.html.erb') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'exposes #foo' do
-    expect(view.foo).to eq exposures.fetch(:foo)
+  it 'exposes #books' do
+    expect(view.books).to eq exposures.fetch(:books)
   end
 end
