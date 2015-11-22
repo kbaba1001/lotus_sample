e119417d422196d8a4fe9f104b93ec1d7118a7ad
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-11-22 13:32:23 +0900

    wip

--- spec/web/views/books/index_spec.rb
+++ spec/web/views/books/index_spec.rb
@@ -9,4 +9,26 @@ describe Web::Views::Books::Index do
   it 'exposes #books' do
     expect(view.books).to eq exposures.fetch(:books)
   end
+
+  describe 'when there are no books' do
+    it 'shows a placeholder message' do
+      expect(rendered).to include('<p class="placeholder">There are no books yet.</p>')
+    end
+  end
+
+  describe 'when there are books' do
+    let(:book1) { Book.new(title: 'Refactoring', author: 'Martin Fowler') }
+    let(:book2) { Book.new(title: 'Domain Driven Design', author: 'Eric Evans') }
+    let(:exposures) { {books: [book1, book2]} }
+
+    it 'lists them all' do
+      expect(rendered.scan(/class="book"/).count).to eq(2)
+      expect(rendered).to include('Refactoring')
+      expect(rendered).to include('Domain Driven Design')
+    end
+
+    it 'hides the placeholder message' do
+      expect(rendered).to include('<p class="placeholder">There are no books yet.</p>')
+    end
+  end
 end
