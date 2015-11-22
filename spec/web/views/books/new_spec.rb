27a020cb61362a1a24b26acae4ea744d7176bb5c
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-11-22 15:35:12 +0900

    show validation error

--- spec/web/views/books/new_spec.rb
+++ spec/web/views/books/new_spec.rb
@@ -1,12 +1,23 @@
 require_relative '../../../../apps/web/views/books/new'
 
+class NewBookParams < Lotus::Action::Params
+  param :book do
+    param :title, presence: true
+    param :author, presence: true
+  end
+end
+
 describe Web::Views::Books::New do
-  let(:exposures) { Hash[foo: 'bar'] }
+  let(:params) { NewBookParams.new({}) }
+  let(:exposures) { {params: params} }
   let(:template)  { Lotus::View::Template.new('apps/web/templates/books/new.html.erb') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'exposes #foo' do
-    expect(view.foo).to eq exposures.fetch(:foo)
+  it 'displays list of errors when params contains errors' do
+    params.valid?
+
+    expect(rendered).to include('There was a problem with your submission')
+    expect(rendered).to include('title is required')
   end
 end
